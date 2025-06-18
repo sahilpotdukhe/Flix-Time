de14714034a4fda02de5f3f7d608b8d17fabea72
sahilpotdukhe <sahilpotdukhe.ssp@gmail.com>
2025-06-18 17:43:28 +0530

    Implemented Repository and wrote logic to store in local db

---
+++ lib/data/repositories/movie_repository.dart
@@ -0,0 +1,8 @@
+import 'package:tmdb_movies/models/movie_model.dart';
+
+abstract class MovieRepository {
+  Future<List<MovieModel>> getTrendingMovies();
+  Future<List<MovieModel>> getNowPlayingMovies();
+  Future<List<MovieModel>> searchMovies(String query);
+  Future<MovieModel> getMovieDetails(int movieId);
+}
